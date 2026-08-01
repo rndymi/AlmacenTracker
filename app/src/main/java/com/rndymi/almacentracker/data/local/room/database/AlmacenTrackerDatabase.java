a028020ac22aeb7d0f1ea10f5fe8da48e0fe8f8f
Randymi <rndymi@gmail.com>
2026-08-01 23:27:52 +0200

    Add non-destructive Room migration for history tables #30

--- app/src/main/java/com/rndymi/almacentracker/data/local/room/database/AlmacenTrackerDatabase.java
+++ app/src/main/java/com/rndymi/almacentracker/data/local/room/database/AlmacenTrackerDatabase.java
@@ -4,15 +4,24 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 import com.rndymi.almacentracker.data.local.room.dao.WarehouseItemDao;
+import com.rndymi.almacentracker.data.local.room.dao.WithdrawalHistoryDao;
 import com.rndymi.almacentracker.data.local.room.entity.WarehouseItemEntity;
+import com.rndymi.almacentracker.data.local.room.entity.WithdrawalHistoryEntity;
+import com.rndymi.almacentracker.data.local.room.entity.WithdrawalHistoryEntryEntity;
 
 @Database(
         entities = {
-                WarehouseItemEntity.class
+                WarehouseItemEntity.class,
+                WithdrawalHistoryEntity.class,
+                WithdrawalHistoryEntryEntity.class
         },
-        version = 1,
+        version = 2,
         exportSchema = true
 )
-public abstract class AlmacenTrackerDatabase extends RoomDatabase {
+public abstract class AlmacenTrackerDatabase
+        extends RoomDatabase {
+
     public abstract WarehouseItemDao warehouseItemDao();
+
+    public abstract WithdrawalHistoryDao withdrawalHistoryDao();
 }
